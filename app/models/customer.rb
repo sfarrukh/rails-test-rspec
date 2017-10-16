cfcfc2387bed7c42d4e36d59f553567569811c01
Admin <Admin@Admins-MacBook-Pro.local>
2017-10-15 20:12:39 -0700

    Test/Add new fields' presence validation in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+
+	validates :first_name, :last_name, :phone_number, :address_id, presence: true
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "is not valid when first_name || last_name || phone_number || address_id is blank" do
+		subject.first_name = 'm'
+		subject.last_name = 'n'
+		subject.phone_number = 1
+		subject.address_id = 1
+		expect(subject).to be_valid
+	end
 end
