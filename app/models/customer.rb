a3fff1ac8aade9be12fe8692227de27aadafe4c9
Admin <Admin@Admins-MacBook-Pro.local>
2017-10-15 22:08:02 -0700

    Table fields' character format & length validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
 
 	validates :first_name, :last_name, :phone_number, :address_id, presence: true
+	validates :first_name, :last_name, length: { maximum: 50 }, format: { with: /\A[a-zA-Z ]+\z/ }
+	validates :phone_number, length: { maximum: 15 }, format: { with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/ }
+	validates :address_id, length: { maximum: 10 }, format: { with:  /\A(\+1)?[0-9]{10}\z/ }
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -18,5 +18,21 @@ RSpec.describe Customer, type: :model do
 		expect(subject).not_to be_valid
 	end
 
+	it "is not valid if first_name or last_name is more than 50 characters" do
+		subject.first_name = 'string'*20	# first_name is too long
+		subject.last_name = "Mercury"
+		subject.phone_number = 9251000000
+		subject.address_id = 2
+		expect(subject).not_to be_valid
+	end
+
+	it "is not valid if one of field's format is not as expected" do
+		subject.first_name = 'Adam'
+		subject.last_name = 'Browns'						
+		subject.phone_number = 9251000000
+		subject.address_id = 'okok'				# address_id should be in integer format
+		expect(subject).not_to be_valid
+	end
+
 
 end
