0d084c96e4c1c54da7798799f9bd40f5468469c4
Admin <Admin@Admins-MacBook-Pro.local>
2017-10-16 21:02:21 -0700

    Test/Define Address table fields' validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
 	has_one :customer
+
+	validates :address, :city, :state, :zipcode, presence: true
+	validates :address, :city, :state, length: { maximum: 60 }, format: { with: /\A[a-zA-Z ]+\z/ }
+	validates :zipcode, length: { maximum: 10 }
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -6,4 +6,36 @@ RSpec.describe Address, type: :model do
   	association = Address.reflect_on_association(:customer)
   	expect(association.macro).to eq(:has_one)
   end
+
+  it "is not valid when one of Address table fields is blank" do
+    subject.address = ""
+    subject.city = "Ruby"
+    subject.state = "Github"
+    subject.zipcode = 94103
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid when one of Customer table fields is nil" do
+    subject.address = "1010 Rspec Dr"
+    subject.city = "Ruby"
+    subject.state = "Github"
+    subject.zipcode = nil
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid if address, city or state is more than 50 characters" do
+    subject.address = "1010 Rspec Dr"
+    subject.city = "Ruby"*50
+    subject.state = "Github"
+    subject.zipcode = nil
+    expect(subject).not_to be_valid
+  end
+
+  it "is not valid if one of field's format is not as expected" do
+    subject.address = 12
+    subject.city = "Ruby"
+    subject.state = "Github"
+    subject.zipcode = nil
+    expect(subject).not_to be_valid
+  end
 end
