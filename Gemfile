26f7baf17043e44317dde98be661338688ca1926
Admin <Admin@Admins-MacBook-Pro.local>
2017-10-17 21:16:11 -0700

    Test rendering templates of Customer/Address Controller

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'bcrypt', '~> 3.1.7'
 group :development, :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
+  gem 'rails-controller-testing'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -3,30 +3,50 @@ require 'rails_helper'
 RSpec.describe CustomersController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    before(:example) do
       get :index
+    end
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+    it "renders index template" do
+      expect(response).to render_template(:index)
     end
   end
 
   describe "GET #new" do
-    it "returns http success" do
+    before(:example) do
       get :new
+    end
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+    it "renders new template" do
+      expect(response).to render_template(:new)
     end
   end
 
   describe "GET #edit" do
-    it "returns http success" do
+    before(:example) do
       get :edit
+    end
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+    it "renders edit template" do
+      expect(response).to render_template(:edit)
     end
   end
 
   describe "GET #delete" do
-    it "returns http success" do
+    before(:example) do
       get :delete
+    end
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+    it "renders delete template" do
+      expect(response).to render_template(:delete)
     end
   end
 
