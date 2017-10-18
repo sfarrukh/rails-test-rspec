e7d0846c0164f3fe0ab7a8690a4f1e1e93dd5cd5
Admin <Admin@Admins-MacBook-Pro.local>
2017-10-17 19:58:26 -0700

    Refactore customer_spec, address_spec to keep DRY principle

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
+
 	has_one :customer
 
 	validates :address, :city, :state, :zipcode, presence: true
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,11 @@
 class Customer < ApplicationRecord
 
-	belongs_to :address
+	has_one :address, foreign_key: 'address_id'
 
 	validates :first_name, :last_name, :phone_number, :address_id, presence: true
 	validates :first_name, :last_name, length: { maximum: 50 }, format: { with: /\A[a-zA-Z ]+\z/ }
-	validates :phone_number, length: { maximum: 15 }, format: { with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/ }
-	validates :address_id, length: { maximum: 10 }, format: { with:  /\A(\+1)?[0-9]{10}\z/ }
+	validates :phone_number, length: { maximum: 15 }
+	validates :address_id, length: { maximum: 10 }
 
 	def name_phone
 		"#{first_name} #{last_name} #{phone_number}"
